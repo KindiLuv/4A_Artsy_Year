9c72f0299235748b7c3760ba0700ab55b6938907
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2023-02-03 16:47:06 +0100

    Ping pong test

--- Assets/Scripts/Component/Player.cs
+++ Assets/Scripts/Component/Player.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ArtsyNetcode;
+using Unity.Netcode;
 
 public class Player : Character
 {
+    [ClientRpc] public void PingClientRPC()
+    {
+        Debug.LogError("pinging server");
+        PongServerRpc(GetComponent<NetworkObject>().OwnerClientId);
+    }
+
+    [ServerRpc] public void PongServerRpc(ulong id)
+    {
+        Debug.LogError($"client id : {id}");
+        PingClientRPC();
+    }
+    
     void Start()
     {
+        if (!IsServer)
+        {
+            return;
+        }
         
+        PingClientRPC();
     }
 
     void Update()
