7e3c433e92cad69321a7de3a4593cba995774be4
mickael-al <mickael.albarello77@gmail.com>
2023-02-19 11:09:28 +0100

    Interactable

---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+
+    public class Interactable : Entity
+    {
+        [Header("Animator")]
+        [SerializeField] protected Animator animator = null;
+        protected bool useState = false;
+        protected bool interactState = false;
+        #region GetterSetter
+        public bool InteractState
+        {
+            get
+            {
+                return interactState;
+            }
+        }
+        #endregion
+
+        public virtual void StartInteract()
+        {
+            useState = true;
+            animator.SetTrigger("Start");
+        }
+        
+        public virtual void Interact(CharacterMove character)
+        {
+            if (useState)
+            {
+                animator.SetTrigger("Use");
+                InteractServerRpc();
+            }            
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void InteractServerRpc()
+        {
+            Debug.Log("");
+        }
+
+        public virtual void StopInteract()
+        {
+            if (useState)
+            {
+                animator.SetTrigger("Stop");
+                useState = false;
+            }
+        }
+
+        public override void OnGameStateChanged(GameState newGameState)
+        {
+            base.OnGameStateChanged(newGameState);
+            if(animator != null)
+            {
+                animator.speed = newGameState == GameState.GamePlay ? 1.0f : 0.0f;
+            }
+        }
+
+
+        public virtual void ChangeInteract(){}
+
+        protected virtual void Start(){}        
+        protected virtual void Update(){}
+    }
+
---
+++ Assets/Scripts/Interactable/PlayerInteract.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class PlayerInteract : MonoBehaviour
+    {
+        [SerializeField] private List<Interactable> interactable = new List<Interactable>();
+        [SerializeField] private float fovInteraction = 60f;
+        private Interactable lastInteraction = null;
+        private PlayerAbiliteControleur playerAbiliteControleur = null;
+        private PlayerInventaire playerInventaire = null;
+        private Player player = null;
+
+        #region GetterSetter
+        public bool CanInteract
+        {
+            get
+            {
+                return interactable.Count > 0;
+            }
+        }
+
+        public bool isInteract
+        {
+            get
+            {
+                return lastInteraction != null ? lastInteraction.InteractState : false;
+            }
+        }
+        #endregion
+
+        private void Start()
+        {
+            playerAbiliteControleur = GetComponent<PlayerAbiliteControleur>();
+            playerInventaire = GetComponent<PlayerInventaire>();
+            player = GetComponent<Player>();
+            InputManager.InputJoueur.Controller.ActionPrincipale.performed += ctx => Interact();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Interactable")
+            {
+                if (other.GetComponent<Interactable>())
+                {
+                    interactable.Add(other.GetComponent<Interactable>());
+                    other.GetComponent<Interactable>().StartInteract();
+                }
+            }
+        }
+        public void ClearInteractable()
+        {
+            foreach (Interactable i in interactable)
+            {
+                if(i!= null)
+                {
+                    i.StopInteract();
+                }
+            }
+            interactable.Clear();
+        }
+
+        public List<Interactable> getInteractable()
+        {
+            return interactable;
+        }
+
+        public void Interact()
+        {            
+            if (playerAbiliteControleur.IsChoising || playerAbiliteControleur.IsUsing || playerInventaire.InInventaire || player.IsAttacking || player.Pause) return;
+            Interactable inter = null;
+            float distance = float.MaxValue;
+            interactable.RemoveAll(i => i == null);
+            List<Interactable> interList = isInView(interactable,fovInteraction);
+            foreach (Interactable i in interList)
+            {
+                if (i.InteractState)
+                {
+                    inter = i;
+                    break;
+                }
+                if (Vector3.Distance(i.gameObject.transform.position, transform.position) < distance)
+                {
+                    distance = Vector3.Distance(i.gameObject.transform.position, transform.position);
+                    inter = i;
+                }
+            }
+            if (inter != null)
+            {
+                if (inter != lastInteraction)
+                {
+                    if (lastInteraction != null)
+                    {
+                        lastInteraction.ChangeInteract();
+                    }
+                }
+                lastInteraction = inter;
+                inter.Interact(this.GetComponent<CharacterMove>());
+            }
+        }
+
+        public List<Interactable> isInView(List<Interactable> list, float fov)
+        {            
+            return list.FindAll(x => x != null &&  Vector3.Angle(x.transform.position - player.ModelCharacter.transform.position, -player.ModelCharacter.transform.forward) < fov || x.InteractState);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Interactable")
+            {
+                if (other.GetComponent<Interactable>())
+                {
+                    other.GetComponent<Interactable>().StopInteract();
+                    interactable.Remove(other.GetComponent<Interactable>());
+                }
+            }
+        }
+    }
