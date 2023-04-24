41f044d35845eaca3efa2ea52e767ca1937e7b1a
Loïc <loicguo@gmail.com>
2023-04-24 18:52:13 +0200

    Character gravity

---
+++ Assets/Scripts/Character/IDamageable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    void TakeDamage();
+    void HealDamage();
+    void DropLoot();
+    void Death();
+    bool isAlive();
+}
--- Assets/Scripts/Component/Character.cs
+++ Assets/Scripts/Component/Character.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 using ArtsyNetcode;
 
 [RequireComponent(typeof(CharacterController))]
-public class Character : NetEntity
+public class Character : NetEntity, IDamageable
 {
+    private float _health;
 
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void HealDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DropLoot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Death()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool isAlive()
+    {
+        throw new System.NotImplementedException();
+    }
 }
